060a04a278d3c786b4dc6cca5b7c0065fcad187a
liuwenlong <1@gitor.org>
2019-10-10 10:23:29 +0800

    修改登录页，修改用户模型

--- account/admin.py
+++ account/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import D4eUser
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(D4eUser)
+
+admin.site.register(D4eUser, UserAdmin)
