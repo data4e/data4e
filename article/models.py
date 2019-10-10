63132873cf1bf63b1ba1983e976925b062932391
liuwenlong <1@gitor.org>
2019-10-10 09:21:20 +0800

    添加模块url

--- article/models.py
+++ article/models.py
@@ -7,7 +7,6 @@ from django.conf import settings
 class BaseModel(models.Model):
     create_time = models.DateTimeField('创建时间', auto_now_add=True)
     update_time = models.DateTimeField('最后更新时间', auto_now=True)
-    id = models.AutoField(primary_key=True)
     delete_flag = (
         (1, '删除'),
         (0, '正常')
@@ -31,6 +30,7 @@ class Articles(BaseModel):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=True)
     tags = models.ManyToManyField('Tags', verbose_name='标签', blank=True)
     body = MDTextField('正文')
+    title = models.CharField(max_length=64, verbose_name='标题')
 
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
