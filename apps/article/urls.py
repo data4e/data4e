6668b129bcb6fbd2fc96fc0e99579b59ff02b985
刘文龙 <1@gitor.org>
2019-10-17 13:52:59 +0800

    新增工具模块

--- apps/article/urls.py
+++ apps/article/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'article'
+app_name = 'articles'
 
 urlpatterns = [
-
+    path('', views.index, name=''),
+    path('add/', views.add, name='add')
 ]
--- apps/article/views.py
+++ apps/article/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'article-list.html')
+
+
+def add(request):
+    if request.method == 'GET':
+        return render(request, 'article-edit.html')
